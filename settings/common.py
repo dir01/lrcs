fd902bef5fbb16ce5aaca76b6798b2427c0da7bc
Andrew Zhu <dir01@dir01.org>
2012-02-23 15:56:50 +0400

    + settings.STATIC_ROOT

--- settings/common.py
+++ settings/common.py
@@ -3,7 +3,9 @@ import os
 __all__ = (
     'PROJECT_ROOT',
     'TEMPLATE_DIR',
+    'STATIC_ROOT',
 )
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 TEMPLATE_DIR = os.path.join(PROJECT_ROOT, 'templates')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
