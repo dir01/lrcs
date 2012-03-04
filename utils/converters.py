0b8ccc9378698bb04ca85b0ec7015e67f391145c
Andrew Zhu <dir01@dir01.org>
2012-03-04 13:33:53 +0400

    + utils.converters.asbool

---
+++ utils/converters.py
@@ -0,0 +1,11 @@
+def asbool(obj):
+    # from paste.deploy.converters
+    if isinstance(obj, (str, unicode)):
+        obj = obj.strip().lower()
+        if obj in ['true', 'yes', 'on', 'y', 't', '1']:
+            return True
+        elif obj in ['false', 'no', 'off', 'n', 'f', '0']:
+            return False
+        else:
+            raise ValueError("String is not true/false: %r" % obj)
+    return bool(obj)
