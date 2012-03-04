b8a0d5f36ebaaeed2683c5c3a4ebf5b2e8e8e7a3
Andrew Zhu <dir01@dir01.org>
2012-03-04 12:43:38 +0400

    renamed webserver modules

--- webserver/site.py
+++ webserver/site.py
@@ -1,14 +1,15 @@
 import os
 import simplejson
 from functools import partial
+from twisted.application import service, internet
 
 from twisted.internet import reactor
 from twisted.web.server import Site, NOT_DONE_YET
 from twisted.web.static import File
 from twisted.web.resource import Resource, NoResource
 
-from jinja2_environment import jinja2_env
-from lyrics import LyricsGainer
+from webserver.jinja2_environment import jinja2_env
+from webserver.lyrics_gainers import LyricsGainer
 from settings import settings
 from utils.memoized import memoized
 
@@ -59,12 +60,12 @@ root.putChild('css', File(os.path.join(settings.STATIC_ROOT, 'css')))
 root.putChild('images', File(os.path.join(settings.STATIC_ROOT, 'images')))
 root.putChild('lyrics', Lyrics())
 
-factory = Site(root)
+site = Site(root)
 
 
 def main():
     print 'Start listening on http://localhost:8080/'
-    reactor.listenTCP(8080, factory)
+    reactor.listenTCP(8080, site)
     reactor.run()
 
 
