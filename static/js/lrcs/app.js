4f2fa80156cf2904144a83becb08152ba1e27496
Алексей Шельф <despawn@gmail.com>
2012-12-23 17:03:37 +0400

    Decouple handling track page from actually showing the track.

--- static/js/lrcs/app.js
+++ static/js/lrcs/app.js
@@ -5,8 +5,8 @@ var lrcs = lrcs || {};
     lrcs.App = Backbone.Router.extend({
 
         routes: {
-            '': 'index',
-            ':artist/:title': 'track', 
+            '': 'visitIndex',
+            ':artist/:title': 'visitTrackPage',
         },
 
         views: {},
@@ -15,7 +15,7 @@ var lrcs = lrcs || {};
             lrcs.lastfm.initialize({ apiKey: lrcs.tools.getMeta('last-fm-api-key') });
             lrcs.dispatch = _.clone(Backbone.Events);
 
-            lrcs.dispatch.on('navigate:track', this.track, this);
+            lrcs.dispatch.on('navigate:track', this.showTrack, this);
 
             this.$el = $('#content');
             this.$title = $('title');
@@ -31,27 +31,25 @@ var lrcs = lrcs || {};
             );
         },
 
-        index: function() {
+        visitIndex: function() {
             this.views.main.hide();
             this.views.lastfm.setInactive();
             this.$title.text('lyri.sk');
         },
 
-        track: function() {
-            var track;
-            if (arguments.length == 2)
-                track = new lrcs.models.Track({
-                    artist: lrcs.tools.decodeURIPart(arguments[0]),
-                    title: lrcs.tools.decodeURIPart(arguments[1])
-                });
-            else
-                track = arguments[0];
+        visitTrackPage: function(artist, title) {
+            var track = new lrcs.models.Track({
+                artist: lrcs.tools.decodeURIPart(artist),
+                title: lrcs.tools.decodeURIPart(title)
+            });
+            this.showTrack(track);
+        },
 
-            this.navigate(track.path());
+        showTrack: function(track) {
+            this.navigate(track.path())
             this.views.main.setModel(track);
             this.views.main.show();
             this.views.lastfm.setActive();
-
             this.$title.text(track.toString());
         }
     
