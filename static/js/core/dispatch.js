dc1e55227eede6e3364862625298bd592a639384
Алексей Шельф <despawn@gmail.com>
2013-02-16 19:15:00 +0400

    Tabs to spaces.
    Tabs to spaces nigga I ain't dumb
    I got 99 problems, but a tab ain't one

--- static/js/core/dispatch.js
+++ static/js/core/dispatch.js
@@ -8,18 +8,18 @@ define([
 
 var Dispatch = _.extend({}, Backbone.Events, {
 
-	NAVIGATE: {
-		INDEX: 'navigate:index',
-		TRACK_PAGE: 'navigate:track'
-	},
+    NAVIGATE: {
+        INDEX: 'navigate:index',
+        TRACK_PAGE: 'navigate:track'
+    },
 
-	visitIndex: function() {
-		this.trigger(this.NAVIGATE.INDEX);
-	},
+    visitIndex: function() {
+        this.trigger(this.NAVIGATE.INDEX);
+    },
 
-	visitTrack: function(track) {
-		this.trigger(this.NAVIGATE.TRACK_PAGE, track);
-	}
+    visitTrack: function(track) {
+        this.trigger(this.NAVIGATE.TRACK_PAGE, track);
+    }
 
 });
 
