b48494caa1421a992a20a6d24721784a49dc1a2f
Алексей Шельф <despawn@gmail.com>
2013-03-16 17:20:50 +0400

    A better (and actually working) implementation of models cache.
    This requires CachedModels to specify their name, as well as
    identifiers.

--- static/js/core/cached-model.js
+++ static/js/core/cached-model.js
@@ -6,28 +6,39 @@ define([
 'use strict';
 
 
+var modelsCache = {};
+
+
 var CachedModel = Backbone.Model.extend({
 
+    name: '',
     identifiers: [],
-    __cache: {},
 
     constructor: function(attributes, options) {
-        if (!(_.isArray(this.identifiers)) || this.identifiers.length == 0)
-            throw new Error('CachedModels have to have identifiers to rely on caching instances.');
-
+        var cache = this.getCache();
         var id = this.getCacheId(attributes);
 
-        if (!(id in this.__cache)) {
+        if (!(id in cache)) {
             Backbone.Model.apply(this, arguments);
-            this.__cache[id] = this;
+            cache[id] = this;
         }
 
-        console.log(this.__cache)
+        return cache[id];
+    },
 
-        return this.__cache[id];
+    getCache: function() {
+        if (!this.name)
+            throw new Error('CachedModels have to have a name to be able to be cached.')
+
+        if (!(this.name in modelsCache))
+            modelsCache[this.name] = {};
+        return modelsCache[this.name]
     },
 
     getCacheId: function(attributes) {
+        if (!(_.isArray(this.identifiers)) || this.identifiers.length == 0)
+            throw new Error('CachedModels have to have identifiers or redefined getCacheId to rely on caching instances.');
+
         var idAttrs = [];
         for (var i = 0, identifier; identifier = this.identifiers[i++];)
             idAttrs.push(attributes[identifier]);
